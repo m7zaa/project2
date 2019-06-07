b471437c5e99035954a2af0300f546c28074caeb
Micah Zaayer <micahzaayer@gmail.com>
2019-06-07 15:57:19 -0700

    add name input and fixed indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,38 @@
 $(document).ready(function() {
+  $("#submitButton").click(function(event){
+    event.preventDefault();
+    // console.log("clicked");
+    var pizza = $("select#pizza").val();
+    // console.log(pizza);
+    var thunderstorms = $("select#thunderstorms").val();
+    // console.log(thunderstorms);
+    var hiking = $("select#hiking").val();
+    var coffee = $("select#coffee").val();
+    var sopranos = $("select#sopranos").val();
+    var name = $("input#name").val();
+    // console.log("name");
+    $(".name").text(name);
+    if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "yes"){
+      // console.log("pizza yes.");
+      $(".programming-language").hide();
+      $("#ruby").show();
+    } else if (pizza === "no" && thunderstorms === "no" && hiking === "no" && coffee === "no" && sopranos === "no"){
+      // console.log("sopranos no");
+      $(".programming-language").hide();
+      $("#python").show();
+    } else {
+      $(".programming-language").hide();
+      $("#rust").show();
+    }
 
 
 
-$("#submitButton").click(function(event){
-  event.preventDefault();
-  console.log("clicked");
-  var pizza = $("select#pizza").val();
-  // console.log(pizza);
-  var thunderstorms = $("select#thunderstorms").val();
-  // console.log(thunderstorms);
-  var hiking = $("select#hiking").val();
-  var coffee = $("select#coffee").val();
-  var sopranos = $("select#sopranos").val();
-
-if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "yes"){
-    console.log("pizza yes.");
-    $(".programming-language").hide();
-    $("#ruby").show();
-} else if (pizza === "no" && thunderstorms === "no" && hiking === "no" && coffee === "no" && sopranos === "no"){
-    console.log("sopranos no");
-    $(".programming-language").hide();
-    $("#python").show();
-} else {
-  $(".programming-language").hide();
-  $("#rust").show();
-}
 
 
 
-
-
-
-//   else if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "no"){
-//     $(".programming-language").hide();
-// }
-
-
-
+    //   else if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "no"){
+    //     $(".programming-language").hide();
+    // }
 
 
 
@@ -65,6 +61,9 @@ if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee ===
 
 
 
-});
+
+
+
+  });
 
 });

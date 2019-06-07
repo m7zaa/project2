6fdafb9eb2422464dcf21d13f557b668a76b37c1
Micah Zaayer <micahzaayer@gmail.com>
2019-06-07 09:57:20 -0700

    add basic if/else branching.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,34 @@ $("#submitButton").click(function(event){
   event.preventDefault();
   console.log("clicked");
   var pizza = $("select#pizza").val();
-  console.log(pizza);
+  // console.log(pizza);
   var thunderstorms = $("select#thunderstorms").val();
-  console.log(thunderstorms);
+  // console.log(thunderstorms);
   var hiking = $("select#hiking").val();
   var coffee = $("select#coffee").val();
   var sopranos = $("select#sopranos").val();
+
+if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "yes"){
+    console.log("pizza yes.");
+    $(".programming-language").hide();
+    $("#ruby").show();
+} else if (pizza === "no" && thunderstorms === "no" && hiking === "no" && coffee === "no" && sopranos === "no"){
+    console.log("sopranos no");
+    $(".programming-language").hide();
+    $("#python").show();
+} else {
+  $(".programming-language").hide();
+  $("#rust").show();
+}
+
+
+
+
+
+
+//   else if (pizza === "yes" && thunderstorms === "yes" && hiking === "yes" && coffee === "yes" && sopranos === "no"){
+//     $(".programming-language").hide();
+// }
 
 
 

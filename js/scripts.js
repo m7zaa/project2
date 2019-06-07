4e9ece8ad1823d308132e617de06321ca1afa19d
Micah Zaayer <micahzaayer@gmail.com>
2019-06-07 09:31:26 -0700

    add colors to buttons and bacground, hidden survey results in html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+
+
+$("#submitButton").click(function(event){
+  event.preventDefault();
+  console.log("clicked");
+  var pizza = $("select#pizza").val();
+  console.log(pizza);
+  var thunderstorms = $("select#thunderstorms").val();
+  console.log(thunderstorms);
+  var hiking = $("select#hiking").val();
+  var coffee = $("select#coffee").val();
+  var sopranos = $("select#sopranos").val();
 
 
 
@@ -7,5 +20,29 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
 });
